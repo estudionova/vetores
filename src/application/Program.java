167171176c1427ec285699ca1b7c4b751bee6e07
Reinaldo Junior <rsljunior@live.com>
2022-02-27 17:04:12 -0300

    Classe finalizada

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,9 +10,19 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        double[] vect = new double[n];
 
-        
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
 
+        double sum = 0.0;
+        for (int i = 0; i < n; i++){
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("AVERAGE HEIGHT:  %.2f%n" , avg);
 
 
         sc.close();
